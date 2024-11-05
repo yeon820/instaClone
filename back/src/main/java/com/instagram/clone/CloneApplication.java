9bfa52011c6e99c69f48d299959010b1f5edba20
김성훈 <167969787+seongffm@users.noreply.github.com>
2024-11-05 20:09:50 +0900

    setting

--- back/src/main/java/com/instagram/clone/CloneApplication.java
+++ back/src/main/java/com/instagram/clone/CloneApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CloneApplication {
 
